b37665756b51da73c5330f3d37d38caf6be8c1fa
yaoxingpu <yaoxpu@163.com>
2024-04-19 10:53:29 +0800

    修改deny.toml配置

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
  * @Author: yaoxingpu yaoxpu@163.com
  * @Date: 2024-04-18 23:19:37
  * @LastEditors: yaoxingpu yaoxpu@163.com
- * @LastEditTime: 2024-04-18 23:42:49
+ * @LastEditTime: 2024-04-19 10:07:12
  * @FilePath: /rcli/src/main.rs
  * @Description:
  *
  */
 fn main() {
-    println!("Hello, rust!");
+    println!("Hello, rust");
 }
