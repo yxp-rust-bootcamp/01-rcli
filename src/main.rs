01be6aa77f23f80b38ffe669026849f71fd1c1c5
yaoxingpu <yaoxpu@163.com>
2024-04-21 00:31:29 +0800

    update: 修改cargo.toml配置

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
  * @Author: yaoxingpu yaoxpu@163.com
  * @Date: 2024-04-18 23:19:37
  * @LastEditors: yaoxingpu yaoxpu@163.com
- * @LastEditTime: 2024-04-20 23:58:58
+ * @LastEditTime: 2024-04-21 00:30:15
  * @FilePath: /rcli/src/main.rs
  * @Description:
  *
@@ -10,6 +10,7 @@
 use clap::Parser;
 use rcli::{process_csv, Opts, SubCommand};
 
+//  cargo run -- csv -i ./assets/juventus.csv
 fn main() -> anyhow::Result<()> {
     println!("Hello, rust");
     let opts = Opts::parse();
